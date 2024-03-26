d0d6104f9b0c3b2ae14a4329e2c796abc6d97f48
Heptazhou <zhou@0h7z.com>
2024-03-26 13:10:55 +0000

    Update

--- workflows.jl
+++ workflows.jl
@@ -21,11 +21,19 @@ macro S_str(string)
 	:(Symbol($string))
 end
 
-const ACT_CHECKOUT() = ODict(
-	S"uses" => S"actions/checkout@v4",
-	S"with" => ODict(S"persist-credentials" => false),
+const ACT_ARTIFACT(pat::String) = ODict(
+	S"uses" => S"actions/upload-artifact@v4",
+	S"with" => ODict(S"compression-level" => 0, S"path" => pat),
 )
-const ACT_SYNC(pkgbase) = ODict(
+const ACT_CHECKOUT(ref::String) = ACT_CHECKOUT(
+	S"path" => Symbol(ref),
+	S"ref"  => Symbol(ref),
+)
+const ACT_CHECKOUT(xs::Pair...) = ODict(
+	S"uses" => S"actions/checkout@v4",
+	S"with" => ODict(S"persist-credentials" => false, xs...),
+)
+const ACT_SYNC(pkgbase::String) = ODict(
 	S"uses" => S"repo-sync/github-sync@v2.3.0",
 	S"with" => ODict(
 		S"source_repo"        => Symbol("https://aur.archlinux.org/$pkgbase.git"),
@@ -34,7 +42,7 @@ const ACT_SYNC(pkgbase) = ODict(
 		S"github_token"       => S"${{ secrets.PAT }}",
 	),
 )
-const JOB_SYNC(pkgbase) = ODict(
+const JOB_SYNC(pkgbase::String) = ODict(
 	S"runs-on" => S"ubuntu-latest",
 	S"steps"   => [ACT_CHECKOUT(), ACT_SYNC(pkgbase)],
 )
@@ -43,7 +51,7 @@ write(".github/workflows/repo-sync.yml",
 	yaml(
 		S"on" => ODict(
 			S"workflow_dispatch" => nothing,
-			S"schedule" => [ODict(S"cron" => "0 */4 * * *")],
+			S"schedule"          => [ODict(S"cron" => "0 */4 * * *")],
 		),
 		S"jobs" => ODict([
 			Symbol(x) => JOB_SYNC(x) for x ∈ [
@@ -57,6 +65,39 @@ write(".github/workflows/repo-sync.yml",
 				"xgterm-bin"
 			]
 		]),
+	),
+)
+
+# https://aur.archlinux.org/packages/nsis
+v = ["mingw-w64-zlib", "nsis"]
+write(".github/workflows/make-nsis.yml",
+	yaml(
+		S"on" => ODict(
+			S"workflow_dispatch" => nothing,
+		),
+		S"jobs" => ODict(
+			S"make" => ODict(
+				S"container" => S"archlinux:base-devel",
+				S"runs-on" => S"ubuntu-latest",
+				S"steps" => [
+					ACT_CHECKOUT.(v);
+					S"run" => "pacman-key --init"
+					S"run" => "pacman -Syu --noconfirm"
+					S"run" => strip(
+						"""
+						makepkg -V
+						sed -ri 's/(EUID) == 0/\\1 < 0/g' /bin/makepkg
+						""" * mapreduce(pkg -> """
+						cd $pkg
+						makepkg -si --noconfirm
+						mv -vt .. *.pkg.tar.zst
+						cd ..
+						""", *, v),
+					)
+					ACT_ARTIFACT("*.pkg.tar.zst")
+				],
+			),
+		),
 	),
 )
 
